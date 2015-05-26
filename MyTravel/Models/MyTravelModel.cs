f1604f36bed3433b2a5cb6cee5be91bdb12afa6c
Juha Kähkönen <juhak75@gmail.com>
2015-05-26 11:17:15 +0300

    Initial version

---
+++ MyTravel/Models/MyTravelModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyTravel.Models
+{
+    public class City
+    {
+        public int CityID { get; set; }
+
+        public string Name { get; set; }
+
+        public int Year { get; set; }
+    }
+}
---
+++ MyTravel/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyTravel.Startup))]
+namespace MyTravel
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
